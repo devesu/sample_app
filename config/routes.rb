43cfa369b954a61efe793fc81c2cc207a01e0479
Devesu <eris@devesu.com>
2016-01-22 21:19:39 +0000

    Make a basic model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'users/new'
 
+  get 'users/new'
+
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
